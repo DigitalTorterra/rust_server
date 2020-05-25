9a9e3aa3a5b519f270b0afadaa33c15daa4eb59c
Your Name <you@example.com>
2020-05-25 01:49:38 -0500

    Added httparse

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
+extern crate threadpool;
+extern crate httparse;
+
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs;
-use server::ThreadPool;
-use std::thread;
-use std::time::Duration;
+use threadpool::ThreadPool;
+//use httparse;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+    
+    let n_workers = 4;
+    let pool = ThreadPool::new(n_workers);
 
-    for stream in listener.incoming().take(2) {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         pool.execute(|| {
@@ -23,16 +27,20 @@ fn main() {
 
 
 fn handle_connection(mut stream: TcpStream) {
+    // Allocate buffers and request
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
+    
+    let mut headers = [httparse::EMPTY_HEADER; 16];
+    let mut req = httparse::Request::new(&mut headers);
+    let res = req.parse(&buffer).unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
-    let sleep = b"GET /sleep HTTP/1.1\r\n";
+    if res.is_partial() {
+        return;
+    }
 
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
-    } else if buffer.starts_with(sleep) {
-        thread::sleep(Duration::from_secs(5));
+    // req fields: method, path, version, headers
+    let (status_line, filename) = if req.path == Some("/") {
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
